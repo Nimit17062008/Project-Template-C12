66ca9a8d5571180c095406ab36c3f85d9210355d
Nimit17062008 <92149487+Nimit17062008@users.noreply.github.com>
2021-11-29 13:03:27 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -37,11 +37,11 @@ function draw() {
    drawSprites();
    
   
-var select_sprites = Math(random(1,3));
+//var select_sprites = Math(random(1,3));
 
 //var select_sprites = Math.random(random(1,3));
 
-//var select_sprites = Math.round(1,3);
+var select_sprites = Math.round(1,3);
 
 // var select_sprites = Math.round(random(1,3));
 
@@ -56,13 +56,13 @@ var select_sprites = Math(random(1,3));
   //   }
   // }
 
-  // if (frameCount % 80 == 0) {
-  //   if (select_sprites == 1) {
-  //     createApples();
-  //   } else if (select_sprites == 2) {
-  //     createOrange();
-  //   }
-  // }
+  if (frameCount % 80 == 0) {
+     if (select_sprites == 1) {
+       createApples();
+     } else if (select_sprites == 2) {
+       createOrange();
+     }
+   }
 
   // if (frameCount / 80 == 0) {
   //   if (select_sprites == 1) {
@@ -74,15 +74,15 @@ var select_sprites = Math(random(1,3));
   //   }
   // }
 
-   if (frameCount % 80 = 0) {
-     if (select_sprites == 1) {
-       createApples();
-     } else if (select_sprites == 2) {
-       createOrange();
-     }else {
-      createRed();
-    }
-  }
+   //if (frameCount % 80 = 0) {
+     //if (select_sprites == 1) {
+       //createApples();
+   //} else if (select_sprites == 2) {
+       //createOrange();
+     //}else {
+      //createRed();
+    //}
+  //}
 
 
 
